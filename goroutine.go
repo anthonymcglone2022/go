9b6eda8a1e961c342663d4283115b9e826f65f72
Anthony McG <102866938+anthonymcglone2022@users.noreply.github.com>
2024-03-20 14:02:28 +0000

    Update goroutine.go

--- goroutine.go
+++ goroutine.go
@@ -1,17 +1,17 @@
 package main
 import (
-   "fmt"
-   "time"
+    "fmt"
+    "time"
 )
 func printIt(msg string) {
-   for i := 0; i < 6; i++ {
-      time.Sleep(300 * time.Millisecond)
-      fmt.Println(msg)
-   }
+    for i := 0; i < 6; i++ {
+        time.Sleep(300 * time.Millisecond)
+        fmt.Println(msg)
+    }
 }
 func main() {
-   // Declare the goroutine
-   go printIt("world")
+    // Declare the goroutine
+    go printIt("world")
  
-   printIt("hello")
+    printIt("hello")
 }
