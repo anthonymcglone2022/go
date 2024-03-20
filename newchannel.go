1c441834f9a30d7cdb08b9bd7111462e745fbef8
Anthony McG <102866938+anthonymcglone2022@users.noreply.github.com>
2024-03-20 14:04:50 +0000

    Update newchannel.go

--- newchannel.go
+++ newchannel.go
@@ -3,42 +3,42 @@ package main
 import "fmt"
 
 type BasicQueue interface {
-	enQueue(val int) string
-	deQueue() string
+    enQueue(val int) string
+    deQueue() string
 }
 
 type channelAsAQueueObject struct {
-	channel chan int
+    channel chan int
 }
 
 func createQueue(size int) BasicQueue {
-	return &channelAsAQueueObject{
-		channel: make(chan int, size),
-	}
+    return &channelAsAQueueObject{
+        channel: make(chan int, size),
+    }
 }
 
 func (a *channelAsAQueueObject) enQueue(value int) string {
-	select {
-		case a.channel <- value:
-			return fmt.Sprintf("Added to Queue %d", value) 
-		default:
-			return "Queue Full"
-	}
+    select {
+        case a.channel <- value:
+            return fmt.Sprintf("Added to Queue %d", value) 
+        default:
+            return "Queue Full"
+    }
 }
 
 func (a *channelAsAQueueObject) deQueue() string {
-	select {
-		case value := <- a.channel:
-			return fmt.Sprintf("Removed from Queue %d", value) 
-		default:
-			return "Queue Empty"
-	}
+    select {
+        case value := <- a.channel:
+            return fmt.Sprintf("Removed from Queue %d", value) 
+        default:
+            return "Queue Empty"
+    }
 }
 
 func main() {
-	channelBackedQueue := createQueue(3)
-	fmt.Println(channelBackedQueue.enQueue(1))
-	fmt.Println(channelBackedQueue.enQueue(2))
-	fmt.Println(channelBackedQueue.enQueue(3))
-	fmt.Println(channelBackedQueue.enQueue(4))
+    channelBackedQueue := createQueue(3)
+    fmt.Println(channelBackedQueue.enQueue(1))
+    fmt.Println(channelBackedQueue.enQueue(2))
+    fmt.Println(channelBackedQueue.enQueue(3))
+    fmt.Println(channelBackedQueue.enQueue(4))
 }
