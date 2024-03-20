77f6272ba188f3cd0849e51b4eebe0f28089c264
Anthony McG <102866938+anthonymcglone2022@users.noreply.github.com>
2024-03-20 14:12:15 +0000

    Update waitgroup.go

--- waitgroup.go
+++ waitgroup.go
@@ -10,27 +10,27 @@ func main() {
     // Declare the WaitGroup
     var wg sync.WaitGroup
 
-	// Create several goroutines
+    // Create several goroutines
     for i := 1; i <= 6; i++ {
 	
-		// Increment the counter
+        // Increment the counter
         wg.Add(1)
 
         i := i
 
         go func() {
-			// Decrement the counter when the goroutine is finished
+	    // Decrement the counter when the goroutine is finished
             defer wg.Done()
             goRoutine(i)
         }()
     }
 
-	// Wait for the counter to go back to 0
+    // Wait for the counter to go back to 0
     wg.Wait()
 }
 
 func goRoutine(id int) {
-	fmt.Println(id, "Routine, started\n")
+    fmt.Println(id, "Routine, started\n")
     time.Sleep(time.Second)
-	fmt.Println(id, "Routine, FINISHED\n")
+    fmt.Println(id, "Routine, FINISHED\n")
 }
