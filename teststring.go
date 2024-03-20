1b28697c912198825bb83c578cad7a72dd5b5b00
Anthony McG <102866938+anthonymcglone2022@users.noreply.github.com>
2024-03-20 14:11:41 +0000

    Update teststring.go

--- teststring.go
+++ teststring.go
@@ -10,28 +10,28 @@ func main() {
     // Declare the WaitGroup
     var wg sync.WaitGroup
 
-	// Create six goroutines
+    // Create six goroutines
     for i := 1; i <= 6; i++ {
 	
-		// Increment the counter
+        // Increment the counter
         wg.Add(1)
 
         i := i
 
         go func() {
-			// Decrement the counter when the goroutine is finished
+	    // Decrement the counter when the goroutine is finished
             defer wg.Done()
 			
             mainFunctionalityInGoRoutine(i)
         }()
     }
 
-	// Wait for the counter to go back to 0
+    // Wait for the counter to go back to 0
     wg.Wait()
 }
 
 func mainFunctionalityInGoRoutine(id int) {
-	fmt.Println(id, "Routine, started\n")
+    fmt.Println(id, "Routine, started\n")
     time.Sleep(time.Second)
-	fmt.Println(id, "Routine, FINISHED\n")
+    fmt.Println(id, "Routine, FINISHED\n")
 }
